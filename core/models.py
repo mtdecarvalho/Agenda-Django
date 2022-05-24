0ee1d96b3c1759bf129d8b5492f64732a2670a2e
Matheus <67925033+mtdecarvalho@users.noreply.github.com>
2022-05-24 20:14:49 -0300

    Primeiro commit

---
+++ core/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+
+
+# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.CharField(max_length=100, blank=True, null=True)
+
+    # auto_now irá inserir automaticamente a datetime atual sempre que um registro for criado
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
+
+    def get_evento_a_comecar(self):
+        if self.data_evento < datetime.now() + timedelta(hours=1):
+            return True
+        else:
+            return False
